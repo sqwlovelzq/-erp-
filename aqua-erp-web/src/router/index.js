5fbbf249c984b3aedcf77defb0d00bc6ec466c23
sqwlovelzq <1904742270@qq.com>
2026-04-05 22:32:30 +0800

    优化了路由文件，去除了重复的路由并改为懒加载

--- aqua-erp-web/src/router/index.js
+++ aqua-erp-web/src/router/index.js
@@ -1,31 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import FarmManage from '../views/FarmManage.vue'
-import ProcessManage from '../views/ProcessManage.vue'
-import ExportManage from '../views/ExportManage.vue'
-import FinanceManage from '../views/FinanceManage.vue' 
-import OrderManage from '../views/OrderManage.vue'
-import Login from '../views/Login.vue' 
-import TracePublic from '../views/TracePublic.vue' 
-import InventoryManage from '../views/InventoryManage.vue'
-
 
 const routes = [
   { path: '/', redirect: '/login' },
-  { path: '/login', name: 'Login', component: Login },
-  { path: '/order', name: 'Order', component: OrderManage },
-  { path: '/farm', name: 'Farm', component: FarmManage },
-  { path: '/process', name: 'Process', component: ProcessManage },
-  { path: '/export', name: 'Export', component: ExportManage },
-  { path: '/finance', name: 'Finance', component: FinanceManage },
-  { path: '/public-trace', name: 'PublicTrace', component: TracePublic } ,
-  { path: '/inventory', name: 'Inventory', component: InventoryManage },
-  {path: '/dashboard',name: 'Dashboard',component: () => import('../views/Dashboard.vue') },
-  {path: '/documents',name: 'DocumentManage',component: () => import('../views/DocumentManage.vue')},
-  {path: '/finance',name: 'FinanceManage',component: () => import('../views/FinanceManage.vue')},
-  {path: '/receivable',name: 'ReceivableManage',component: () => import('../views/ReceivableManage.vue')},
-  {path: '/documents',name: 'DocumentManage',component: () => import('../views/DocumentManage.vue')}
+  { path: '/login', name: 'Login', component: () => import('../views/Login.vue') },
+  { path: '/dashboard', name: 'Dashboard', component: () => import('../views/Dashboard.vue') },
+  { path: '/order', name: 'Order', component: () => import('../views/OrderManage.vue') },
+  { path: '/farm', name: 'Farm', component: () => import('../views/FarmManage.vue') },
+  { path: '/process', name: 'Process', component: () => import('../views/ProcessManage.vue') },
+  { path: '/export', name: 'Export', component: () => import('../views/ExportManage.vue') },
+  { path: '/finance', name: 'Finance', component: () => import('../views/FinanceManage.vue') },
+  { path: '/receivable', name: 'ReceivableManage', component: () => import('../views/ReceivableManage.vue') },
+  { path: '/documents', name: 'DocumentManage', component: () => import('../views/DocumentManage.vue') },
+  { path: '/inventory', name: 'Inventory', component: () => import('../views/InventoryManage.vue') },
+  { path: '/public-trace', name: 'PublicTrace', component: () => import('../views/TracePublic.vue') }
 ]
-
 const router = createRouter({history: createWebHistory(),routes})
 
 // 登录拦截守卫
